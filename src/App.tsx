1b807975b43ef654b0c2612a411d69d49f358889
Benja <christianbfc97@gmail.com>
2022-09-25 16:53:49 -0500

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,36 @@
 import * as React from 'react'
-import { SERVICE } from './constants/constants'
+import {GoogleOAuthProvider, GoogleLogin} from "@react-oauth/google";
+import axios from "axios";
+import {Simulate} from "react-dom/test-utils";
+
+function handleSuccess(response: any) {
+  const token = response.credential;
+
+  axios.post('auth/google', {
+    token: token
+  }).then((response) => {
+    console.log(response)
+  }).catch((error) => {
+    console.log(error)
+  })
+}
+
 
 function App() {
+  const googleClientId = process.env.REACT_APP_GOOGLE_CLIENT_ID
+
   return (
     <>
       <h1>TicketPE</h1>
+      <GoogleOAuthProvider clientId={googleClientId}>
+        <GoogleLogin
+          onSuccess={handleSuccess}
+          onError={() => {
+            console.log('Login Failed');
+          }}
+          useOneTap
+        />
+      </GoogleOAuthProvider>
     </>
   )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,10 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom/client'
 import App from './App'
+import axios from "axios";
+
+
+axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
